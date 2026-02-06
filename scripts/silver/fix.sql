dd94d236f89a41183db13825f8e887447142974f
Anudeep Marlapalli <63209848+Anudeep423@users.noreply.github.com>
2026-02-06 12:23:56 +0530

    Update fix.sql

--- scripts/silver/fix.sql
+++ scripts/silver/fix.sql
@@ -191,3 +191,27 @@ case
 end as cntry
 FROM bronze.erp_loc_a101;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+insert into silver.erp_px_cat_g1v2(
+id,
+cat,
+subcat,
+maintenance)
+select *
+from bronze.erp_px_cat_g1v2;
+
